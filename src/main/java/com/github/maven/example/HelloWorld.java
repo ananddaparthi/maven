20451970aec67df3ab425177f9898159428aedb4
ananddaparthi <anand.daparthi1@gmail.com>
2017-03-19 23:25:11 -0400

    Update HelloWorld.java

--- src/main/java/com/github/maven/example/HelloWorld.java
+++ src/main/java/com/github/maven/example/HelloWorld.java
@@ -5,7 +5,7 @@ import org.joda.time.LocalTime;
 public class HelloWorld {
 	public static void main(String[] args) {
 		LocalTime currentTime = new LocalTime();
-		System.out.println("The curbbbgrejjhkbhjnt locaaaal utttttimmmme i:s: " + currentTime);
+		System.out.println("The curbbbgrejjhdkbhjnt locaaaal utttttimmmme i:s: " + currentTime);
 		Greeter greeter = new Greeter();
 		System.out.println(greeter.sayHello());
 	}
